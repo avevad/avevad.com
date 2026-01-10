1866aedf10874eef2dcdaded5a8fedbf43bd59bb
Vadim Averin <vadim@avevad.com>
2026-01-10 05:26:12 +0300

    Rework balancer configuration (stage 3)

--- NITROGEN/nixos/balancer/configuration.nix
+++ NITROGEN/nixos/balancer/configuration.nix
@@ -8,17 +8,8 @@ in
   services.haproxy = {
     enable = true;
     config = (builtins.replaceStrings
-      [
-        "@CERT_FILE_PUSHY@"
-        "@CERT_FILE_PRO@"
-        "@DEPLOY_TOKEN@"
-      ]
-      [
-        "${./haproxy_pushy.pem}"
-        "${./haproxy_pro.pem}"
-        ENV.TOKENS.HAPROXY_DEPLOY
-      ]
-      (builtins.readFile ./haproxy.cfg)
+        [ "@DEPLOY_TOKEN@" ] [ ENV.TOKENS.HAPROXY_DEPLOY ]
+        (builtins.readFile ./haproxy.cfg)
     );
   };
 }
